db5e94d8aff0e48b1189a7cc3ea5a805c695b89e
S. Apulu <59923507+S-Apulu3008@users.noreply.github.com>
2020-04-08 13:08:45 -0400

    Add files via upload
    falling distance calc

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*2.15.2020
+ * CSC-153-0001
+ * Sene Apulu
+ * Falling distance calculator
+ */
+
+
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = "";
+            double fallTime, //fall time in seconds
+                   fallDist; //distance fell in meters
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Falling Distance Calculator");
+                Console.WriteLine("---------------------------\n");
+                Console.Write("enter falling time (in seconds): ");
+                Double.TryParse(Console.ReadLine(), out fallTime);          //get object fall time(seconds).
+                fallDist = FallingDistance(fallTime);                       //calculate fall distance(meters)
+                Console.WriteLine($"\nobject fell: {fallDist} meters");     //display falling distance.
+
+                Console.WriteLine("\n\n[enter] continue | [q] quit");       //loop break;
+                userInput = Console.ReadLine();
+            } while (userInput != "q");
+
+        }
+        static double FallingDistance(double seconds) 
+        {
+            double g = 9.8;
+            double distance = .5 * (g * (seconds * seconds)); // distance = 1/2 gt^2            
+            return distance;
+        }
+    }
+}
